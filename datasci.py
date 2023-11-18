0819052b3433d4249e657b768016c8e008a455f6
Jenny Tsai <jennytsai@gwu.edu>
2023-11-18 16:21:30 -0500

    missing value report
    missing values and percentage

--- datasci.py
+++ datasci.py
@@ -9,7 +9,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class datasci():
-    pass
+    def __init__(self, df):
+        self.df = df
+        self.shape = df.shape
+        self.nrows = df.shape[0]
+        self.ncols = df.shape[1]
+
+    def size(self):
+        return f"There are {self.nrows} rows and {self.ncols} columns in the dataset."
+        
+    def missingReport(self):
+        missingVal = self.df[self.df.columns[self.df.isnull().any()].tolist()].isnull().sum()
+        missingPer = round((missingVal * 100/ self.nrows), 2)
+
+        frame = {'Num of Nans': missingVal,
+                'Percent of Nans': missingPer}
+
+        missing_report = pd.DataFrame(frame).sort_values(by='Percent of Nans', ascending = False)
+
+        print(f'The following variables contains over 80% missing values and are recommended to be removed:')
+        print(missing_report[missing_report['Percent of Nans']>=80].index.tolist())
+        print('')
+
+        return missing_report
 
 
 # %%
